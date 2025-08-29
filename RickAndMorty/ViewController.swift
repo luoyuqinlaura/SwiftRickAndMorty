0783988f3d8c2437b2ffe0f8d078968c04f3c684
yuqinluo <yuqin_luo@apple.com>
2025-08-29 15:28:11 -0700

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Yuqin Luo on 8/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
